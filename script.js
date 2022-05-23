5775029e3496c7281a8caecc5880c6d2abbe83a4
kscode <kashinggo@gmail.com>
2022-05-24 00:19:51 +0800

    fix typo in css, add image url in html

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const panels = document.querySelectorAll(".panels");
+const panels = document.querySelectorAll(".panel");
 
-panels.map((panel) => {
-  panel.addEventListen("click", () => {
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
     removeActiveClasses();
     panel.classList.add("active");
   });
 });
 
 const removeActiveClasses = () => {
-  panels.map((panel) => {
+  panels.forEach((panel) => {
     panel.classList.remove("active");
   });
 };
